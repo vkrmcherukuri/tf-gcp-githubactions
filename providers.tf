1acce432bd2b75bb03f236697e5207de03609014
Vikram Cherukuri <vkrmcherukuri@gmail.com>
2023-05-21 14:45:51 -0400

    test

--- providers.tf
+++ providers.tf
@@ -3,3 +3,10 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+terraform {
+  backend "gcs" {
+    bucket = "round-bounty-363611-tfstate"
+    prefix = "terraform/state"
+  }
+}
